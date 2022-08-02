04a380848bc799829b1c1d0dc7856111d4a569af
Hothuankhang <khang.ho@madison-technology.com>
2022-08-02 17:32:52 +0700

    Slide

--- footer.php
+++ footer.php
@@ -1,3 +1,137 @@
+<section class="info_section layout_padding">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-3">
+          <h5>
+            Fruits
+          </h5>
+          <ul>
+            <li>
+              randomised
+            </li>
+            <li>
+              words which
+            </li>
+            <li>
+              don't look even
+            </li>
+            <li>
+              slightly
+            </li>
+            <li>
+              believable. If you
+            </li>
+            <li>
+              are going to use
+            </li>
+            <li>
+              a passage of
+            </li>
+            <li>
+              Lorem Ipsum,
+            </li>
+          </ul>
+        </div>
+        <div class="col-md-3">
+          <h5>
+            Services
+          </h5>
+          <ul>
+            <li>
+              randomised
+            </li>
+            <li>
+              words which
+            </li>
+            <li>
+              don't look even
+            </li>
+            <li>
+              slightly
+            </li>
+            <li>
+              believable. If you
+            </li>
+            <li>
+              are going to use
+            </li>
+            <li>
+              a passage of
+            </li>
+            <li>
+              Lorem Ipsum,
+            </li>
+          </ul>
+        </div>
+        <div class="col-md-3">
+          <h5>
+            List
+          </h5>
+          <ul>
+            <li>
+              randomised
+            </li>
+            <li>
+              words which
+            </li>
+            <li>
+              don't look even
+            </li>
+            <li>
+              slightly
+            </li>
+            <li>
+              believable. If you
+            </li>
+            <li>
+              are going to use
+            </li>
+            <li>
+              a passage of
+            </li>
+            <li>
+              Lorem Ipsum,
+            </li>
+          </ul>
+        </div>
+        <div class="col-md-3">
+          <div class="social_container">
+            <h5>
+              Follow Us
+            </h5>
+            <div class="social-box">
+              <a href="">
+                <img src="<?php the_field('fb', 'option'); ?>" alt="">
+              </a>
+
+              <a href="">
+                <img src="<?php the_field('twitter', 'option'); ?>" alt="">
+              </a>
+              <a href="">
+                <img src="<?php the_field('linkedin', 'option'); ?>" alt="">
+              </a>
+              <a href="">
+                <img src="<?php the_field('instagram', 'option'); ?>" alt="">
+              </a>
+            </div>
+          </div>
+          <div class="subscribe_container">
+            <h5>
+              Subscribe Now
+            </h5>
+            <div class="form_container">
+              <form action="">
+                <input type="email">
+                <button type="submit">
+                  Subscribe
+                </button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
 <!-- footer section -->
 <section class="container-fluid footer_section">
     <p>
